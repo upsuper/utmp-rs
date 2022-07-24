9e4c8fea8bcd763e062b4be020cdb74b916092f9
Xidorn Quan <me@upsuper.org>
2022-07-24 14:53:29 +1000

    Improve usage document

--- README.md
+++ README.md
@@ -9,11 +9,26 @@ A Rust crate for parsing `utmp` files like `/var/run/utmp` and `/var/log/wtmp`.
 
 ## Usage
 
+Simplest way is to use `parse_from_*` functions,
+which returns a `Vec<UtmpEntry>` on success:
 ```rust
-fn main() -> Result<()> {
-    let entries = utmp_rs::parse_from_path("/var/run/utmp")?;
+let entries = utmp_rs::parse_from_path("/var/run/utmp")?;
+// ...
+```
+
+If you don't need to collect them all,
+`UtmpParser` can be used as an iterator:
+```rust
+use utmp_rs::UtmpParser;
+for entry in UtmpParser::from_path("/var/run/utmp")? {
+    let entry = entry?;
     // ...
 }
 ```
+
+All the `parse_from_*` functions as well as `UtmpParser` parse `utmp` file
+based on the native format for the target platform.
+If cross-platform parsing is needed,
+`Utmp32Parser` or `Utmp64Parser` can be used instead of `UtmpParser`.
 
 <!-- cargo-sync-readme end -->
--- src/lib.rs
+++ src/lib.rs
@@ -2,14 +2,35 @@
 //!
 //! ## Usage
 //!
-//! ```rust
-//! # use anyhow::Result;
-//! fn main() -> Result<()> {
-//!     let entries = utmp_rs::parse_from_path("/var/run/utmp")?;
-//!     // ...
-//! #   Ok(())
-//! }
+//! Simplest way is to use `parse_from_*` functions,
+//! which returns a `Vec<UtmpEntry>` on success:
 //! ```
+//! # use anyhow::Result;
+//! # fn main() -> Result<()> {
+//! let entries = utmp_rs::parse_from_path("/var/run/utmp")?;
+//! // ...
+//! # Ok(())
+//! # }
+//! ```
+//!
+//! If you don't need to collect them all,
+//! `UtmpParser` can be used as an iterator:
+//! ```
+//! # use anyhow::Result;
+//! use utmp_rs::UtmpParser;
+//! # fn main() -> Result<()> {
+//! for entry in UtmpParser::from_path("/var/run/utmp")? {
+//!     let entry = entry?;
+//!     // ...
+//! }
+//! # Ok(())
+//! # }
+//! ```
+//!
+//! All the `parse_from_*` functions as well as `UtmpParser` parse `utmp` file
+//! based on the native format for the target platform.
+//! If cross-platform parsing is needed,
+//! `Utmp32Parser` or `Utmp64Parser` can be used instead of `UtmpParser`.
 
 mod entry;
 mod parse;
