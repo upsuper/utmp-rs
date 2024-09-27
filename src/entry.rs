bd7129cc7d9a1b4cfa9bfbba479d1578744631df
Honsun Zhu <honsun@linux.com>
2024-09-27 20:50:13 +0800

    feat: add some fields to UtmpEntry (#7)
    Co-authored-by: Xidorn Quan <me@upsuper.org>

--- src/entry.rs
+++ src/entry.rs
@@ -1,7 +1,7 @@
 use libc::pid_t;
 use std::convert::TryFrom;
-use std::ffi::CStr;
 use std::os::raw::c_short;
+use std::str;
 use thiserror::Error;
 use time::OffsetDateTime;
 use utmp_raw::x32::utmp as utmp32;
@@ -15,6 +15,8 @@ pub enum UtmpEntry {
     Empty,
     /// Change in system run-level (see `init(8)`)
     RunLevel {
+        /// PID of the init process
+        pid: pid_t,
         /// Kernel version
         kernel_version: String,
         /// Time entry was made
@@ -49,6 +51,12 @@ pub enum UtmpEntry {
     LoginProcess {
         /// PID of the login process
         pid: pid_t,
+        /// Device name of tty
+        line: String,
+        /// Username
+        user: String,
+        /// Hostname for remote login
+        host: String,
         /// Time entry was made
         time: OffsetDateTime,
     },
@@ -123,6 +131,7 @@ impl<'a> TryFrom<&'a utmp64> for UtmpEntry {
                     }
                 } else {
                     UtmpEntry::RunLevel {
+                        pid: from.ut_pid,
                         kernel_version,
                         time,
                     }
@@ -141,6 +150,9 @@ impl<'a> TryFrom<&'a utmp64> for UtmpEntry {
             utmp_raw::LOGIN_PROCESS => UtmpEntry::LoginProcess {
                 pid: from.ut_pid,
                 time: time_from_tv(from.ut_tv)?,
+                line: string_from_bytes(&from.ut_line).map_err(UtmpError::InvalidLine)?,
+                user: string_from_bytes(&from.ut_user).map_err(UtmpError::InvalidUser)?,
+                host: string_from_bytes(&from.ut_host).map_err(UtmpError::InvalidHost)?,
             },
             utmp_raw::USER_PROCESS => UtmpEntry::UserProcess {
                 pid: from.ut_pid,
@@ -186,13 +198,11 @@ fn time_from_tv(tv: timeval64) -> Result<OffsetDateTime, UtmpError> {
 }
 
 fn string_from_bytes(bytes: &[u8]) -> Result<String, Box<[u8]>> {
-    bytes
-        .iter()
-        .position(|b| *b == 0)
-        .and_then(|pos| {
-            // This is safe because we manually located the first zero byte above.
-            let cstr = unsafe { CStr::from_bytes_with_nul_unchecked(&bytes[..=pos]) };
-            Some(cstr.to_str().ok()?.to_string())
-        })
-        .ok_or_else(|| bytes.to_owned().into_boxed_slice())
+    let trimmed = match bytes.iter().position(|b| *b == 0) {
+        Some(pos) => &bytes[..pos],
+        None => bytes,
+    };
+    str::from_utf8(trimmed)
+        .map(|s| s.into())
+        .map_err(|_| bytes.into())
 }
--- tests/basic.rs
+++ tests/basic.rs
@@ -21,6 +21,7 @@ fn get_basic32_expected() -> Vec<UtmpEntry> {
             time: timestamp(1581199438_054727_000),
         },
         UtmpEntry::RunLevel {
+            pid: 53,
             kernel_version: "5.3.0-29-generic".to_owned(),
             time: timestamp(1581199447_558900_000),
         },
@@ -43,6 +44,264 @@ fn get_basic32_expected() -> Vec<UtmpEntry> {
         UtmpEntry::LoginProcess {
             pid: 28965,
             time: timestamp(1581217268_463588_000),
+            line: "tty4".to_owned(),
+            user: "LOGIN".to_owned(),
+            host: "".to_owned(),
+        },
+    ]
+}
+
+fn get_with_host_32_expected() -> Vec<UtmpEntry> {
+    vec![
+        UtmpEntry::ShutdownTime {
+            kernel_version: "5.4.0-135-generic".to_owned(),
+            time: timestamp(1672223597_077918_000),
+        },
+        UtmpEntry::BootTime {
+            kernel_version: "5.4.0-135-generic".to_owned(),
+            time: timestamp(1675756860_150698_000),
+        },
+        UtmpEntry::RunLevel {
+            pid: 53,
+            kernel_version: "5.4.0-135-generic".to_owned(),
+            time: timestamp(1675756874_594747_000),
+        },
+        UtmpEntry::InitProcess {
+            pid: 627,
+            time: timestamp(1675756875_303010_000),
+        },
+        UtmpEntry::InitProcess {
+            pid: 644,
+            time: timestamp(1675756875_305313_000),
+        },
+        UtmpEntry::LoginProcess {
+            pid: 644,
+            line: "tty1".to_owned(),
+            user: "LOGIN".to_owned(),
+            host: "".to_owned(),
+            time: timestamp(1675756875_305313_000),
+        },
+        UtmpEntry::LoginProcess {
+            pid: 627,
+            line: "ttyS0".to_owned(),
+            user: "LOGIN".to_owned(),
+            host: "".to_owned(),
+            time: timestamp(1675756875_303010_000),
+        },
+        UtmpEntry::UserProcess {
+            pid: 1125,
+            line: "pts/0".to_owned(),
+            user: "root".to_owned(),
+            host: "112.124.2.209".to_owned(),
+            session: 0,
+            time: timestamp(1675757226_139552_000),
+        },
+        UtmpEntry::UserProcess {
+            pid: 1127,
+            line: "pts/1".to_owned(),
+            user: "root".to_owned(),
+            host: "112.124.2.209".to_owned(),
+            session: 0,
+            time: timestamp(1675757226_284647_000),
+        },
+        UtmpEntry::DeadProcess {
+            pid: 1020,
+            line: "pts/0".to_owned(),
+            time: timestamp(1675757226_404205_000),
+        },
+        UtmpEntry::DeadProcess {
+            pid: 1020,
+            line: "pts/1".to_owned(),
+            time: timestamp(1675757227_275375_000),
+        },
+        UtmpEntry::UserProcess {
+            pid: 1225,
+            line: "pts/0".to_owned(),
+            user: "root".to_owned(),
+            host: "112.124.2.209".to_owned(),
+            session: 0,
+            time: timestamp(1675757312_920719_000),
+        },
+        UtmpEntry::UserProcess {
+            pid: 2454,
+            line: "pts/1".to_owned(),
+            user: "root".to_owned(),
+            host: "".to_owned(),
+            session: 0,
+            time: timestamp(1675758317_098468_000),
+        },
+        UtmpEntry::UserProcess {
+            pid: 2714,
+            line: "pts/1".to_owned(),
+            user: "root".to_owned(),
+            host: "".to_owned(),
+            session: 0,
+            time: timestamp(1675758522_887514_000),
+        },
+        UtmpEntry::DeadProcess {
+            pid: 1189,
+            line: "pts/0".to_owned(),
+            time: timestamp(1675759743_147069_000),
+        },
+        UtmpEntry::UserProcess {
+            pid: 4343,
+            line: "pts/0".to_owned(),
+            user: "root".to_owned(),
+            host: "112.124.2.209".to_owned(),
+            session: 0,
+            time: timestamp(1675759955_391532_000),
+        },
+        UtmpEntry::UserProcess {
+            pid: 5022,
+            line: "pts/1".to_owned(),
+            user: "root".to_owned(),
+            host: "".to_owned(),
+            session: 0,
+            time: timestamp(1675760619_783753_000),
+        },
+        UtmpEntry::DeadProcess {
+            pid: 4305,
+            line: "pts/0".to_owned(),
+            time: timestamp(1675761785_613258_000),
+        },
+        UtmpEntry::UserProcess {
+            pid: 13369,
+            line: "pts/0".to_owned(),
+            user: "root".to_owned(),
+            host: "112.124.2.209".to_owned(),
+            session: 0,
+            time: timestamp(1675768806_832709_000),
+        },
+    ]
+}
+
+fn get_long_user_32_expected() -> Vec<UtmpEntry> {
+    vec![
+        UtmpEntry::LoginProcess {
+            pid: 1872475,
+            line: "pts/1".to_owned(),
+            user: "abc".to_owned(),
+            host: "".to_owned(),
+            time: timestamp(1675278673_563046_000),
+        },
+        UtmpEntry::LoginProcess {
+            pid: 1874257,
+            line: "pts/1".to_owned(),
+            user: "abc".to_owned(),
+            host: "".to_owned(),
+            time: timestamp(1675278942_329935_000),
+        },
+        UtmpEntry::LoginProcess {
+            pid: 1875352,
+            line: "ssh:notty".to_owned(),
+            user: "abc".to_owned(),
+            host: "10.11.0.169".to_owned(),
+            time: timestamp(1675279200_000000_000),
+        },
+        UtmpEntry::LoginProcess {
+            pid: 1875352,
+            line: "ssh:notty".to_owned(),
+            user: "abc".to_owned(),
+            host: "10.11.0.169".to_owned(),
+            time: timestamp(1675279205_000000_000),
+        },
+        UtmpEntry::LoginProcess {
+            pid: 1875352,
+            line: "ssh:notty".to_owned(),
+            user: "abc".to_owned(),
+            host: "10.11.0.169".to_owned(),
+            time: timestamp(1675279206_000000_000),
+        },
+        UtmpEntry::LoginProcess {
+            pid: 2199784,
+            line: "ssh:notty".to_owned(),
+            user: "aaaaaaaaaa".to_owned(),
+            host: "10.10.4.230".to_owned(),
+            time: timestamp(1675423140_000000_000),
+        },
+        UtmpEntry::LoginProcess {
+            pid: 2199784,
+            line: "ssh:notty".to_owned(),
+            user: "aaaaaaaaaa".to_owned(),
+            host: "10.10.4.230".to_owned(),
+            time: timestamp(1675423143_000000_000),
+        },
+        UtmpEntry::LoginProcess {
+            pid: 2199784,
+            line: "ssh:notty".to_owned(),
+            user: "aaaaaaaaaa".to_owned(),
+            host: "10.10.4.230".to_owned(),
+            time: timestamp(1675423148_000000_000),
+        },
+        UtmpEntry::LoginProcess {
+            pid: 2200630,
+            line: "ssh:notty".to_owned(),
+            user: "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa".to_owned(),
+            host: "10.10.4.230".to_owned(),
+            time: timestamp(1675423317_000000_000),
+        },
+        UtmpEntry::LoginProcess {
+            pid: 2200630,
+            line: "ssh:notty".to_owned(),
+            user: "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa".to_owned(),
+            host: "10.10.4.230".to_owned(),
+            time: timestamp(1675423321_000000_000),
+        },
+        UtmpEntry::LoginProcess {
+            pid: 2200630,
+            line: "ssh:notty".to_owned(),
+            user: "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa".to_owned(),
+            host: "10.10.4.230".to_owned(),
+            time: timestamp(1675423325_000000_000),
+        },
+        UtmpEntry::LoginProcess {
+            pid: 2200630,
+            line: "ssh:notty".to_owned(),
+            user: "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa".to_owned(),
+            host: "10.10.4.230".to_owned(),
+            time: timestamp(1675423330_000000_000),
+        },
+        UtmpEntry::LoginProcess {
+            pid: 2203029,
+            line: "ssh:notty".to_owned(),
+            user: "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa".to_owned(),
+            host: "10.10.4.230".to_owned(),
+            time: timestamp(1675424016_000000_000),
+        },
+        UtmpEntry::LoginProcess {
+            pid: 2203029,
+            line: "ssh:notty".to_owned(),
+            user: "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa".to_owned(),
+            host: "10.10.4.230".to_owned(),
+            time: timestamp(1675424020_000000_000),
+        },
+        UtmpEntry::LoginProcess {
+            pid: 2203029,
+            line: "ssh:notty".to_owned(),
+            user: "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa".to_owned(),
+            host: "10.10.4.230".to_owned(),
+            time: timestamp(1675424024_000000_000),
+        },
+        UtmpEntry::LoginProcess {
+            pid: 2203029,
+            line: "ssh:notty".to_owned(),
+            user: "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa".to_owned(),
+            host: "10.10.4.230".to_owned(),
+            time: timestamp(1675424031_000000_000),
+        },
+        UtmpEntry::LoginProcess {
+            pid: 2214635,
+            line: "ssh:notty".to_owned(),
+            user: "bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb".to_owned(),
+            host: "10.10.4.230".to_owned(),
+            time: timestamp(1675424626_000000_000),
+        },
+        UtmpEntry::LoginProcess {
+            pid: 2214635,
+            line: "ssh:notty".to_owned(),
+            user: "bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb".to_owned(),
+            host: "10.10.4.230".to_owned(),
+            time: timestamp(1675424630_000000_000),
         },
     ]
 }
@@ -54,12 +313,16 @@ fn get_basic64_expected() -> Vec<UtmpEntry> {
             time: timestamp(1658083371_314869_000),
         },
         UtmpEntry::RunLevel {
+            pid: 53,
             kernel_version: "5.15.0-41-generic".to_owned(),
             time: timestamp(1658083400_855073_000),
         },
         UtmpEntry::LoginProcess {
             pid: 1219,
             time: timestamp(1658083400_866391_000),
+            line: "ttyAMA0".to_owned(),
+            user: "LOGIN".to_owned(),
+            host: "".to_owned(),
         },
     ]
 }
@@ -69,6 +332,22 @@ fn parse_basic32() -> Result<()> {
     let path = SAMPLES_PATH.join("basic32.utmp");
     let actual = Utmp32Parser::from_path(&path)?.collect::<Result<Vec<_>, _>>()?;
     let expected = get_basic32_expected();
+    Ok(assert_eq!(actual, expected))
+}
+
+#[test]
+fn parse_with_host_32() -> Result<()> {
+    let path = SAMPLES_PATH.join("with_host_32.utmp");
+    let actual = Utmp32Parser::from_path(&path)?.collect::<Result<Vec<_>, _>>()?;
+    let expected = get_with_host_32_expected();
+    Ok(assert_eq!(actual, expected))
+}
+
+#[test]
+fn parse_long_user_32() -> Result<()> {
+    let path = SAMPLES_PATH.join("long_user_32.utmp");
+    let actual = Utmp32Parser::from_path(&path)?.collect::<Result<Vec<_>, _>>()?;
+    let expected = get_long_user_32_expected();
     Ok(assert_eq!(actual, expected))
 }
 
